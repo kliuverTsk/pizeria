e3c7809d5ef9f2381b412e7053e195fe7effd48b
kliuverTsk <kliuvertfigueroa@gmail.com>
2025-04-13 14:05:35 -0300

    Configuración simple

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pizeria/',
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      output: {
-        format: 'systemjs',
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    },
-    modulePreload: {
-      polyfill: true
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  }
+  base: '/pizeria/'
 })
